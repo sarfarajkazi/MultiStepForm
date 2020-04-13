d4893622f7e2112fdbd47e0078fb65af64069668
Sarfaraj Kazi <sarfarajkazi7@gmail.com>
2020-04-13 15:00:46 +0530

    Enable Google analytics ID

--- assets/js/MSF_front_scripts.js
+++ assets/js/MSF_front_scripts.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function ($) {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('create', 'UA-160196817-3', 'auto');
     //UA-160196817-3
     ga('send', 'FormView');
 
